a94d3150e52d98f13ffc37520d78d27297b4f74e
daixian <amano_tooko@qq.com>
2019-05-30 16:23:24 +0800

    修改更新最新的dll

---
+++ Assets/Plugins/GCSeries/PenRay.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+
+namespace GCSeries
+{
+    /// <summary>
+    /// 绘制一条从笔尖发出的射线
+    /// </summary>
+    internal class PenRay : MonoBehaviour
+    {
+        /// <summary>
+        /// 笔的射线的LineRenderer
+        /// </summary>
+        private LineRenderer _lineRenderer;
+
+        /// <summary>
+        /// 射线的最长长度,规定为1m
+        /// </summary>
+        public static float MAX_RAY_LENGTH = 1.0f;
+
+        private void Awake()
+        {
+            _lineRenderer = gameObject.AddComponent<LineRenderer>();
+            _lineRenderer.material = new Material(Shader.Find("Unlit/Texture"));
+        }
+
+        private void Update()
+        {
+
+            float rayLength = MAX_RAY_LENGTH * FCore.ViewerScale;//射线初始设置长度1m长
+
+            _lineRenderer.SetPosition(0, FCore.penPosition);
+            if (FCore.isDraging)//如果当前正在拖拽状态
+            {
+                //使用记录的拖拽距离来设置射线长
+                _lineRenderer.SetPosition(1, FCore.penPosition + FCore.lastDragDistance * FCore.penDirection.normalized);
+            }
+            else//如果不在拖拽状态
+            {
+                RaycastHit raycastHit;
+                int layer = LayerMask.NameToLayer("Default");
+                if (Physics.Raycast(FCore.penRay, out raycastHit, rayLength, 1 << layer))//使用设定的射线长度来做射线检测
+                {
+                    _lineRenderer.SetPosition(1, raycastHit.point);
+                }
+                else
+                {
+                    _lineRenderer.SetPosition(1, FCore.penPosition + (rayLength * FCore.penDirection.normalized));
+                }
+            }
+            _lineRenderer.startWidth = 0.001f * FCore.ViewerScale;//画出的射线的宽度
+            _lineRenderer.endWidth = 0.001f * FCore.ViewerScale;
+        }
+
+        private void OnDestroy()
+        {
+            Destroy(_lineRenderer.material);
+        }
+    }
+}
---
+++ Assets/Plugins/GCSeries/SimpleDrag.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GCSeries
+{
+    /// <summary>
+    /// 简单拖拽的示例，首先在Start()中绑定事件监听操控笔的按下抬起动作。
+    /// 当操控笔按钮0按下的时候，可以开始一个物体的拖拽，在拖拽的同时时候按动按钮1，按钮2，
+    /// 可以进行拖拽物体的拉近拉远操作。
+    /// </summary>
+    public class SimpleDrag : MonoBehaviour
+    {
+
+        /// <summary>
+        /// 创建的笔的射线物体
+        /// </summary>
+        GameObject _penObj;
+
+        void Start()
+        {
+
+            //设置屏幕为3D显示模式
+            FCore.SetScreen3D();
+
+            FCore.EventKey0Down += OnKey0Down;
+            FCore.EventKey0Up += OnKey0Up;
+            FCore.EventKey1Down += OnKey1Down;
+            FCore.EventKey2Down += OnKey2Down;
+
+            _penObj = new GameObject("penRay");
+            _penObj.AddComponent<PenRay>();
+        }
+
+        private void OnApplicationQuit()
+        {
+            //在程序退出的时候设置屏幕为2D显示
+            FCore.SetScreen2D();
+        }
+
+        /// <summary>
+        /// 是否在点击的时候震动一下
+        /// </summary>
+        public bool enableShake = true;
+
+        /// <summary>
+        /// 记录当前拖拽的物体
+        /// </summary>
+        GameObject _curDragObj;
+
+        ///-------------------------------------------------------------------------------------------------
+        /// <summary>
+        /// 按键0按下的事件响应.
+        /// </summary>
+        ///
+        /// <remarks> Dx, 2017/9/22. </remarks>
+        ///-------------------------------------------------------------------------------------------------
+        private void OnKey0Down()
+        {
+            RaycastHit raycastHit;
+            GameObject dragObj = Raycast(out raycastHit);
+            if (dragObj != null)
+            {
+                _curDragObj = dragObj;//记录当前拖拽物体
+                FCore.addDragObj(raycastHit.collider.gameObject, raycastHit.distance);
+                if (enableShake)
+                {
+                    FCore.PenShake();//震动一下
+                }
+            }
+        }
+
+        ///-------------------------------------------------------------------------------------------------
+        /// <summary>
+        /// 按键0抬起的事件响应.
+        /// </summary>
+        ///
+        /// <remarks> Dx, 2017/9/22. </remarks>
+        ///-------------------------------------------------------------------------------------------------
+        private void OnKey0Up()
+        {
+            FCore.deleteDragObj(_curDragObj);
+        }
+
+        ///-------------------------------------------------------------------------------------------------
+        /// <summary>
+        /// 按键1按下的事件响应.
+        /// </summary>
+        ///
+        /// <remarks> Dx, 2018/3/5. </remarks>
+        ///-------------------------------------------------------------------------------------------------
+        private void OnKey1Down()
+        {
+            if (FCore.isDraging)
+            {
+                FCore.pushDragObj(_curDragObj, 0.10f);
+            }
+        }
+
+        ///-------------------------------------------------------------------------------------------------
+        /// <summary>
+        /// 按键2按下的事件响应.
+        /// </summary>
+        ///
+        /// <remarks> Dx, 2018/3/5. </remarks>
+        ///-------------------------------------------------------------------------------------------------
+        private void OnKey2Down()
+        {
+            if (FCore.isDraging)
+            {
+                FCore.pullDragObj(_curDragObj, 0.10f);
+            }
+        }
+
+        private void Update()
+        {
+            if (FCore.isDraging)
+            {
+                //响应两个按键长按的功能
+                if (FCore.isKey1Down)
+                    FCore.pushDragObj(_curDragObj, 1.50f * Time.deltaTime);
+                if (FCore.isKey2Down)
+                    FCore.pullDragObj(_curDragObj, 1.50f * Time.deltaTime);
+            }
+
+        }
+
+        ///-------------------------------------------------------------------------------------------------
+        /// <summary>
+        /// 简单的检测，如果有物体则返回物体，否则返回null.
+        /// </summary>
+        ///
+        /// <remarks> Dx, 2017/9/22. </remarks>
+        ///
+        /// <param name="raycastHit"> [out] The raycast hit. </param>
+        ///
+        /// <returns> A GameObject. </returns>
+        ///-------------------------------------------------------------------------------------------------
+        private GameObject Raycast(out RaycastHit raycastHit)
+        {
+            int layer = LayerMask.NameToLayer("Default");
+            float rayLength = FCore.ViewerScale * PenRay.MAX_RAY_LENGTH;//规定射线拿取的最大长度为1m，这里可以自由规定
+            if (Physics.Raycast(FCore.penRay, out raycastHit, rayLength, 1 << layer))
+            {
+                return raycastHit.collider.gameObject;
+            }
+            return null;
+        }
+    }
+}
