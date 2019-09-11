cb202f2af0dd0b57dad7b16a433ff40dc4face2d
keyso <keyso000@outlook.com>
2019-09-11 17:45:01 +0800

    增加了打包脚本

---
+++ Assets/tools/Editor/OutputPackage.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+namespace GCSeries
+{
+    public class OutputPackage:Editor
+    {
+        static string outputPath = System.IO.Directory.GetParent(Application.dataPath).FullName + "/Output";
+        [MenuItem("GCSeries/Export GCSeries SDK")]
+        static void ExportGCSeries()
+        {
+            if (!Directory.Exists(outputPath))
+            {
+                Directory.CreateDirectory(outputPath);
+            }
+            var assetPathNames = new string[] { "Assets/GCSeries", "Assets/StreamingAssets" };
+            AssetDatabase.ExportPackage(assetPathNames, outputPath + "/GCSeries.unitypackage", ExportPackageOptions.Interactive | ExportPackageOptions.Recurse);
+        }
+    }
+}
